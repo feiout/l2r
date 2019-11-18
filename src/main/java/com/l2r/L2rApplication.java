becf08b14e6816fe951947e4397888624a10109a
messi <52486846@qq.com>
2019-11-18 13:12:13 +0800

    L2R Project

---
+++ src/main/java/com/l2r/L2rApplication.java
@@ -0,0 +1,13 @@
+package com.l2r;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L2rApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L2rApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/l2r/L2rApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l2r;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L2rApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
